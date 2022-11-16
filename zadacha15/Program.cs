6ca0e988312f4d675e221e9edd0ac81bef9e7fb3
maxbondarenko <bondarenkomm@bk.ru>
2022-11-16 21:14:32 +0500

    zadacha15

---
+++ zadacha15/Program.cs
@@ -0,0 +1,23 @@
+﻿/*Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+и проверяет, является ли этот день выходным.*/
+
+System.Console.Write("Введите номер дня недели: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1 || number > 7){
+         Console.Write("Такого дня недели не существует, введите корректное число: ");
+         number = Convert.ToInt32(Console.ReadLine());}
+            if (number < 1 && number > 7)
+               Console.Write("Ошибка ввода");
+
+       
+else 
+    if (number == 6){
+    System.Console.WriteLine("Это суббота!");
+}
+    if (number == 7){
+    System.Console.WriteLine("Это воскресенье!");
+}
+    if (number >=1 && number <=5){
+    System.Console.WriteLine("Это будний день(((");
+}
