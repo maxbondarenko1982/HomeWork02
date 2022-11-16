541bf2385947b3127685887e3043f33f4bc693a5
maxbondarenko <bondarenkomm@bk.ru>
2022-11-16 21:10:41 +0500

    zadacha10

---
+++ zadacha10/Program.cs
@@ -0,0 +1,14 @@
+﻿/*Задача 10: Напишите программу, которая принимает на вход трёхзначное число 
+и на выходе показывает вторую цифру этого числа.*/
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number > 99 && number < 999)
+{
+    number = number%100/10;
+    Console.WriteLine(number);
+}
+else
+{
+    System.Console.WriteLine("Неверное число");
+}
